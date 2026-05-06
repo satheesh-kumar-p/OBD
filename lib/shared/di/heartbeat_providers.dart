d2405fdf1a141e42e0b6cd13121a67108c9a355e
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-05-06 19:58:24 +0530

    Add heartbeat use case and stream providers

--- lib/shared/di/heartbeat_providers.dart
+++ lib/shared/di/heartbeat_providers.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:scout_obd/core/di/injection_container.dart';
 import 'package:scout_obd/core/logger/logger.dart';
+import 'package:scout_obd/shared/application/use_cases/watch_heartbeat_use_case.dart';
 import 'package:scout_obd/shared/data/repositories/heartbeat_repository_impl.dart';
+import 'package:scout_obd/shared/domain/entities/heartbeat_entity.dart';
 import 'package:scout_obd/shared/domain/repositories/heartbeat_repository.dart';
 
 final heartbeatLoggerProvider = Provider<Logger>((ref) => Logger('HEARTBEAT'));
@@ -14,4 +16,14 @@ final heartbeatRepositoryProvider = Provider<HeartbeatRepository>((ref) {
     commManager: commManager,
     logger: logger,
   );
+});
+
+final watchHeartbeatUseCaseProvider = Provider<WatchHeartbeatUseCase>((ref) {
+  return WatchHeartbeatUseCase(ref.watch(heartbeatRepositoryProvider));
+});
+
+final heartbeatProvider =
+StreamProvider.family<HeartbeatEntity, String>((ref, linkId) {
+  final useCase = ref.watch(watchHeartbeatUseCaseProvider);
+  return useCase(linkId);
 });
