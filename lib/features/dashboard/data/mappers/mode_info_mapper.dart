65cc90db05234ca0cddc729ca13ec8df57c40624
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-08 11:50:36 +0530

    Update CAN-related imports in system and dashboard mappers
    - Update `can_field.dart` import path to reflect its new location in `core/comm/can_bus`
    - Add `can_extraction_strategy.dart` import to `system_info_mapper.dart` and `mode_info_mapper.dart`

--- lib/features/dashboard/data/mappers/mode_info_mapper.dart
+++ lib/features/dashboard/data/mappers/mode_info_mapper.dart
@@ -1,4 +1,5 @@
-import '../../../../shared/data/can_field.dart';
+import '../../../../core/comm/can_bus/can_extraction_strategy.dart';
+import '../../../../core/comm/can_bus/can_field.dart';
 import '../../domain/entities/mode_entity.dart';
 import '../../enums/mode_enum.dart';
 
