0d6b785bfcd26d908e450c639b9bdec5b13c55fc
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-09 10:05:43 +0530

    Delete messages from features

--- lib/features/dashboard/enums/e_stop_status_enum.dart
+++
@@ -1,9 +0,0 @@
-enum EStopStatus {
-  unknown(-1, 'UNKNOWN'),
-  released(0, 'RELEASED'),
-  engaged(1, 'ENGAGED');
-
-  const EStopStatus(this.value, this.label);
-  final int value;
-  final String label;
-}
