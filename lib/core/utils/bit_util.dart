d2888d87252e72ff4c601aadb7cd9a45c24a9c6d
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-10 15:16:39 +0530

    Change bit_util.dart to scan bits from right to left. (ICD Update)

--- lib/core/utils/bit_util.dart
+++ lib/core/utils/bit_util.dart
@@ -3,28 +3,21 @@ import 'dart:typed_data';
 /// Utility to extract bits from a CAN payload.
 class BitUtil {
   /// Extracts a value from [data] starting at [startBit] with [length] bits.
-  /// Logic: Big Endian - Bit 0 is MSB, Bit 7 is LSB.
   static int getBitsByLength(Uint8List data, int startBit, int length) {
     if (startBit < 0 || length < 0) {
       throw ArgumentError('startBit and length must be non-negative');
     }
 
     int value = 0;
-    String bitString = '';
     for (int i = 0; i < length; i++) {
       int bitPos = startBit + i;
       int byteIdx = bitPos ~/ 8;
-      // If Bit 0 is MSB, then bit 0 in a byte corresponds to bit 7 in standard shifting (0x80)
-      int bitIdx = 7 - (bitPos % 8);
+      int bitIdx = bitPos % 8;
 
       if (byteIdx >= data.length) break;
 
-      // Extract the bit (from MSB towards LSB)
       int bit = (data[byteIdx] >> bitIdx) & 0x01;
-      bitString += bit.toString();
-      
-      // Since startBit is MSB, as i increases, significance decreases
-      value |= (bit << (length - 1 - i));
+      value |= (bit << i);
     }
     return value;
   }
