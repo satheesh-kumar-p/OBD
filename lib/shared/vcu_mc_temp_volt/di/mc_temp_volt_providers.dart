67a982dc99f25a07084202cbb187d3a03ba3f340
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-12 11:50:34 +0530

    Integrate vcu_mc_temp_volt message

---
+++ lib/shared/vcu_mc_temp_volt/di/mc_temp_volt_providers.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/di/injection_container.dart';
+import '../data/repositories/mc_temp_volt_repository.dart';
+import '../domain/entities/mc_temp_volt_entity.dart';
+import '../../../core/logger/logger.dart';
+
+final mcTempVoltLoggerProvider =
+Provider<Logger>((ref) => Logger('MC_TEMP_VOLT'));
+
+final mcTempVoltRepoProvider =
+Provider<McTempVoltRepository>((ref) {
+  final logger = ref.read(mcTempVoltLoggerProvider);
+  final commManager = ref.watch(canManagerProvider);
+
+  return McTempVoltRepository(
+    canManager: commManager,
+    logger: logger,
+  );
+});
+
+final mcTempVoltProvider =
+StreamProvider<McTempVoltEntity>((ref) {
+  final repository = ref.watch(mcTempVoltRepoProvider);
+
+  repository.startCanData();
+
+  ref.onDispose(() {
+    repository.stopCanData();
+  });
+
+  return repository.watchCanData();
+});
