f4ae134dc3b3325e67d471cf1d0e83db568ab1ae
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-12 10:57:18 +0530

    Remove time controller from dashboard providers

--- lib/features/dashboard/di/dashboard_providers.dart
+++ lib/features/dashboard/di/dashboard_providers.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import '../application/dashboard_controller.dart';
-import '../application/time_controller.dart';
 import '../state/dashboard_state.dart';
 
 /// Manages the currently selected tab in the dashboard.
