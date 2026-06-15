18c936c985f3a785cdcf39bd37c66206334e1bf2
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-15 15:20:48 +0530

    Update drive info repository to use CommManager

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/application/core_controller.dart';
-import 'core/constants/app_constants.dart';
-import 'core/di/injection_container.dart';
 import 'features/dashboard/presentation/screens/dashboard.dart';
 import 'features/dashboard/presentation/widgets/app_background.dart';
 
