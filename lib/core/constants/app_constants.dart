ba2b16eb3182cc96b99723b05a0cd7f7bf82780a
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-16 09:26:47 +0530

    Remove global time message from dispatch configurations

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -22,10 +22,7 @@ abstract final class AppConstants {
 
   // Centralized Message Dispatch Rules
   static const List<MessageConfig> dispatchConfigs = [
-    // Global Time Info (0x202) - Pass-through (Low frequency)
-    MessageConfig.passThrough(0x202),
-
-    // Time Sync (0x206) - 10Hz for smooth clock
+    // Time Sync (0x206)
     MessageConfig.sample(0x206, 2.0),
   ];
 }
