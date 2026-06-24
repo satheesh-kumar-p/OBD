21b466976fe8af36e2c26d42f42e9c0d42a2b873
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-24 15:31:55 +0530

    Add LV PDU health entities and enums

---
+++ lib/shared/vcu_power_subsystem_health/domain/entities/power_health_entity.dart
@@ -0,0 +1,12 @@
+import 'hv_battery_health_entity.dart';
+import 'lv_pdu_health_entity.dart';
+
+class PowerHealthEntity {
+  final HvBatteryHealthEntity hvBattery;
+  final LvPduHealthEntity lvPdu;
+
+  const PowerHealthEntity({
+    required this.hvBattery,
+    required this.lvPdu,
+  });
+}
