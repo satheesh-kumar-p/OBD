7df5e9bdb77f9fb42388b02753959aa41b2a021f
Karthikeya Tummala <tummala.karthikeya@newspace.co.in>
2026-06-29 09:26:02 +0530

    Fix motor controller mapping in SystemInfoMapper

--- lib/shared/vcu_subsystem_state/data/mapper/system_info_mapper.dart
+++ lib/shared/vcu_subsystem_state/data/mapper/system_info_mapper.dart
@@ -98,8 +98,8 @@ class SystemInfoMapper extends CanExtractionStrategy<SystemInfoEntity> {
   SystemInfoEntity build(Map<String, dynamic> values) {
     return SystemInfoEntity(
       // Motor controllers: ICD rear→left, front→right mapping
-      frontMotorController: _toStatus(values['rearMotorController']),
-      rearMotorController: _toStatus(values['frontMotorController']),
+      frontMotorController: _toStatus(values['frontMotorController']),
+      rearMotorController: _toStatus(values['rearMotorController']),
 
       // Other components
       hvBattery: _toStatus(values['hvBattery']),
